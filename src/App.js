0b9f2997ee22151c8d6c03aa45bb90e37fd6fbe6
Hongshan <zhou.hongshan636@gmail.com>
2021-08-16 18:33:04 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Header extends React.Component {
+  render(){
+  return <h1>Welcome to Ironhack Cinema</h1>
+}
+}
+
+class Main extends React.Component {
+  render(){
+  return (
+    <main>
+      <Movie title="Mission Impossible" />
+      <Movie title="The Matrix" />
+      <Movie title="Inception" />
+    </main>
+  );
+  }
+}
+
+class Movie extends React.Component {
+  render(){
+  return (
+    <section className='movie'>
+      <h5>{this.props.title}</h5>
+    </section>
+  )
+  }
+}
+
+class Footer extends React.Component {
+  render(){
+  return <footer>By Team Ironhack</footer>
+}
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Main />
+    <Footer />
     </div>
   );
 }
