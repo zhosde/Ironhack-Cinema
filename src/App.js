5bedac3f8ca59005fb1d77cfc72c2465adfbbbec
Hongshan <zhou.hongshan636@gmail.com>
2021-08-17 11:41:01 +0200

    like btn

--- src/App.js
+++ src/App.js
@@ -21,10 +21,25 @@ class Main extends React.Component {
 }
 
 class Movie extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      numberOfLikes: 0
+    }
+  }
+    clickBtn = () => {
+      this.setState(prevState => {
+        return {
+          numberOfLikes: prevState.numberOfLikes+1
+        }
+      })
+    }
   render(){
   return (
     <section className='movie'>
       <h5>{this.props.title}</h5>
+      <p>Number of Likes: {this.state.numberOfLikes}</p>
+      <button onClick={this.clickBtn}>Like it!</button>
     </section>
   )
   }
